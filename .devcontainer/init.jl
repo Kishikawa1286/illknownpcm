b35b6b8b45163d515ae34bcb8da29a446ddab36a
Kentaro Ishikawa <kentaroishikawa1286@gmail.com>
2024-04-11 00:04:18 +0000

    パッケージのバージョン固定

--- .devcontainer/init.jl
+++ .devcontainer/init.jl
@@ -1,19 +1,21 @@
 using Pkg
 
 function main()
-    Pkg.add([
-        "JuMP",
-        "HiGHS",
-        "IntervalArithmetic",
-        "Printf",
-        "Latexify",
-        "LaTeXStrings",
-        "Plots",
-        "PyPlot",
-        "Distributions",
-        "ProgressMeter",
-        "StatsPlots"
-        ])
+    Pkg.add(
+        Pkg.PackageSpec[
+            Pkg.PackageSpec(name="Distributions", version="0.25.100"),
+            Pkg.PackageSpec(name="HiGHS", version="1.6.0"),
+            Pkg.PackageSpec(name="IntervalArithmetic", version="0.21.0"),
+            Pkg.PackageSpec(name="JuMP", version="1.14.1"),
+            Pkg.PackageSpec(name="LaTeXStrings"),
+            Pkg.PackageSpec(name="Latexify"),
+            Pkg.PackageSpec(name="Plots", version="1.15.2"),
+            Pkg.PackageSpec(name="Printf"),
+            Pkg.PackageSpec(name="ProgressMeter"),
+            Pkg.PackageSpec(name="PyPlot", version="2.11.2"),
+            Pkg.PackageSpec(name="StatsPlots", version="0.14.33")
+        ]
+    )
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
